3c6b0bca4fd93abbdb813e6c4353f7ef0329df28
Eva Luna Álvarez <zap363@amerike.edu.mx>
2023-03-18 23:35:45 -0600

    Started Flutter UI Page | UI Background Colors Ready

---
+++ android/app/src/main/kotlin/com/imperatorstudios/loginscreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imperatorstudios.loginscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF73AEF5),
+                  Color(0xFF61A4F1),
+                  Color(0xFF478DE0),
+                  Color(0xFF398AE5),
+                ],
+                stops: [
+                  0.1,
+                  0.4,
+                  0.7,
+                  0.9
+                ],
+              )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
